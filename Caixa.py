e7155072595249bfb74e90849fd1d616a195e6e3
Master <mateluci_pl@hotmail.com>
2021-10-31 01:48:17 -0300

    pilha finalizado

--- Caixa.py
+++ Caixa.py
@@ -15,9 +15,18 @@ class Caixa:
     def vazia(self):
         return len(self.clientes) == 0
 
-    def set_nome(self, nome):
-        self.nome = nome
+    def set_nome(self, nome, caixas : list):
+        p = False
+        for caixa in caixas:
+            if caixa.get_nome() == nome:
+                print("O caixa {} já existe".format(nome))
+                p = True
 
+        if p == False:
+            print("O caixa {} foi criado com sucesso".format(nome))
+            self.nome = nome
+    
+      
     def get_nome(self):
         return self.nome
 
@@ -26,59 +35,7 @@ class Caixa:
 
     def abrir(self):
         self.estado = 'aberto'
-        
-    def fechar(self, caixas : list, nome_caixa : str):
-        count = len(caixas)
-        ultimo = caixas[count - 1]
-        
-        if ultimo.get_nome() == nome_caixa.get_nome():
-            ultimo.fechar_Caixa()
-            return "Caixa fechar com sucesso"
-        
-        if ultimo.get_estado_Caixa() == 'aberto':
-            return "Favor fechar o caixa " + ultimo.get_nome()
-
-        t = 0
-        p = -2
-        o = count * 2
-        count = count - o - 1
-        print(count)
-        print(p)
-        while p > count:
-            print("este é o caixa de p",caixas[p].get_nome())
-            print("este é o caixa de nome",nome_caixa.get_nome())
-            if caixas[p].get_nome() == nome_caixa.get_nome():
-                if caixas[t].get_estado_Caixa() == 'aberto':
-                    return "Favor fechar o caixa : "+ caixas[t].get_nome()
-                else:
-                    caixas[p].fechar_Caixa()
-                    return "Caixa fechar com sucesso"
-            p-=1
-            t+=1
-            print("este é o P",p)
-            print("este é o T",t)
-        # i =0
-        # emAberto = []
-        # for itens in caixas:
-        #     i+=1
-        #     if itens.get_nome() == nome_caixa.get_nome():
-        #         total = count - i
-        #         print(total)
-        #         while total < count:
-        #             print("este é o total",total)
-        #             print("este é o p",p)
-        #             print(caixas[total].get_nome())
-        #             emAberto.append(caixas[total])
-        #             total+=1
-        #         p = ''
-        #         for itens in emAberto:
-        #             p = str(itens.get_nome())+": "+ p
-        #         if emAberto == []:
-        #             nome_caixa.fechar_Caixa()
-        #             return "Caixa fechado"
-        #         else:
-        #             return "Favor fechar esses caixas primeiro"+" "+p
-
+    
     def fechar_Caixa(self):
         self.estado = 'fechado'
 
@@ -90,3 +47,31 @@ class Caixa:
     
     def __repr__(self):
         return str(self.__dict__)
+
+    def __str__(self):
+        return "O caixa {} está com {} clientes".format(self.nome, len(self.clientes))
+
+    def fechar(self, caixas : list, nome_caixa : str):
+        count = len(caixas)
+        ultimo = caixas[count - 1]
+
+        if ultimo.get_nome() == nome_caixa.get_nome():
+            ultimo.fechar_Caixa()
+            return "O... Caixa {} foi fechado com sucesso".format(nome_caixa.get_nome())
+        
+        if ultimo.get_estado_Caixa() == 'aberto':
+            return "Favor fechar o caixa " + ultimo.get_nome()
+
+        t = -1
+        p = -2
+        o = count * 2
+        count = count - o - 1
+        while p > count:
+            if caixas[p].get_nome() == nome_caixa.get_nome():
+                if caixas[t].get_estado_Caixa() == 'aberto':
+                    return "Favor fechar o caixa : "+ caixas[t].get_nome()
+                else:
+                    caixas[p].fechar_Caixa()
+                    return "O Caixa {} foi fechado com sucesso".format(caixas[p].get_nome())
+            p-=1
+            t-=1
--- Main.py
+++ Main.py
@@ -6,13 +6,10 @@ from Produto import Produto
 def main():
     pessoa1 = Pessoa()
     pessoa2 = Pessoa()
-    pessoa3 = Pessoa()
-    pessoa4 = Pessoa()
 
     caixa1 = Caixa()
     caixa2 = Caixa()
     caixa3 = Caixa()
-    caixa4 = Caixa()
 
     produto1 = Produto()
     produto2 = Produto()
@@ -21,28 +18,18 @@ def main():
 
     pessoa1.set_nome("João")
     pessoa2.set_nome("Maria")
-    pessoa3.set_nome("Pedro")
-    pessoa4.set_nome("José")
 
-    caixa1.set_nome("Caixa 1")
-    caixa2.set_nome("Caixa 2")
-    caixa3.set_nome("Caixa 3")
-    caixa4.set_nome("Caixa 4")
-    
+    caixas = [caixa1, caixa2, caixa3]
+    caixa1.set_nome("Caixa 1", caixas)
+    caixa2.set_nome("Caixa 2", caixas)
+    caixa3.set_nome("Caixa 3", caixas)
+
     caixa1.entrar([pessoa1])
     caixa2.entrar([pessoa2])
-    caixa3.entrar([pessoa3])
-    caixa4.entrar([pessoa4])
 
     caixa1.abrir()
     caixa2.abrir()
     caixa3.abrir()
-    caixa4.abrir()
-
-    print("Quantidade de pessoas no caixa: {}".format(caixa1.__len__()))
-    print("Quantidade de pessoas no caixa: {}".format(caixa2.__len__()))
-    print("Quantidade de pessoas no caixa: {}".format(caixa3.__len__()))
-    print("Quantidade de pessoas no caixa: {}".format(caixa4.__len__()))
 
     produto1.set_nome("Coca-cola")
     produto2.set_nome("Doritos")
@@ -61,21 +48,17 @@ def main():
 
     pessoa1.compras(compra1)
     pessoa2.compras(compra2)
-    pessoa3.compras(compra3)
-    pessoa4.compras(compra4)
+
+    # print("O {}, comprou esses itens {} no caixa {} e o total da compra foi de R${}".format(pessoa1.get_nome(), pessoa1.get_compras(), caixa1.get_nome(), pessoa1.total()))
+    # print("O {}, comprou esses itens {} no caixa {} e o total da compra foi de R${}".format(pessoa2.get_nome(), pessoa2.get_compras(), caixa2.get_nome(), pessoa2.total()))
+
+    caixas = [caixa1,caixa2,caixa3]
+    print(caixa3.fechar(caixas, caixa3))
     
-    pessoa3.remover_produto("Nesgresco")
-    print("O {}, comprou esses itens {} no caixa {} e o total da compra foi de R${}".format(pessoa1.get_nome(), pessoa1.get_compras(), caixa1.get_nome(), pessoa1.total()))
-    print("O {}, comprou esses itens {} no caixa {} e o total da compra foi de R${}".format(pessoa2.get_nome(), pessoa2.get_compras(), caixa2.get_nome(), pessoa2.total()))
-    print("O {}, comprou esses itens {} no caixa {} e o total da compra foi de R${}".format(pessoa3.get_nome(), pessoa3.get_compras(), caixa3.get_nome(), pessoa3.total()))
-    print("O {}, comprou esses itens {} no caixa {} e o total da compra foi de R${}".format(pessoa4.get_nome(), pessoa4.get_compras(), caixa4.get_nome(), pessoa4.total()))
-
-    caixas = [caixa1,caixa2,caixa3,caixa4]
-    caixa4.fechar(caixas, caixa4)
-    # caixa3.fechar(caixas, caixa3)
-    # caixa2.fechar(caixas, caixa2)
-    p = caixa1.fechar(caixas, caixa1)
-    print(p)
-
+    print(caixa2.fechar(caixas, caixa2))
+    
+    print(caixa1.fechar(caixas, caixa1))
+   
+    
 if __name__ == "__main__":
     main()
